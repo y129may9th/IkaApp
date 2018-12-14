9f678afb38d345cbe8803665c585215275bf6b40
y129may9th <y129may9th@gmail.com>
2018-12-14 20:19:54 +0900

    Fix Gem DB

--- app/controllers/ikabot_controller.rb
+++ app/controllers/ikabot_controller.rb
@@ -31,10 +31,6 @@ class IkabotController < ApplicationController
             text: event.message['text']
           }
           client.reply_message(event['replyToken'], message)
-        when Line::Bot::Event::MessageType::Image, Line::Bot::Event::MessageType::Video
-          response = client.get_message_content(event.message['id'])
-          tf = Tempfile.open("content")
-          tf.write(response.body)
         end
       end
     }
